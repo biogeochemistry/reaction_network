dd76a5f0d43450860439607e50a3ff7be44c7699
pycckuu <is.markelov@gmail.com>
2014-04-18 01:40:20 +0200

    Assert min < max

--- FiniteDifferenceGrid.cpp
+++ FiniteDifferenceGrid.cpp
@@ -4,17 +4,19 @@
 using namespace Eigen;
 
 void FiniteDifferenceGrid::xGridForamtion(int xNumNodes, double xMin, double xMax){
+    assert(xMin < xMax);
     double stepsize = (xMax-xMin)/(xNumNodes-1);
     xGrid.resize(xNumNodes);
-    for(unsigned i = 0; i < xNumNodes; ++i) {
+    for(int i = 0; i < xNumNodes; ++i) {
         xGrid(i) =  xMin+i*stepsize;
     }
 }
 
 void FiniteDifferenceGrid::yGridFormation(int yNumNodes, double yMin, double yMax){
+    assert(yMin < yMax);
     double stepsize = (yMax-yMin)/(yNumNodes-1);
     yGrid.resize(yNumNodes);
-    for(unsigned i = 0; i < yNumNodes; ++i) {
+    for(int i = 0; i < yNumNodes; ++i) {
         yGrid(i) =  yMin+i*stepsize;
     }
 }
